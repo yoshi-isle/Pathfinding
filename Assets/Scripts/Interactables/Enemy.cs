a74836db752d64a95879f477c92e325b34f8d1be
Tangy <joshua.jaworowski@gmail.com>
2025-03-09 20:55:34 -0400

    aggro range stuff

--- Assets/Scripts/Interactables/Enemy.cs
+++ Assets/Scripts/Interactables/Enemy.cs
@@ -1,14 +1,74 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+
 public class Enemy : Interactable
 {
     public bool AggroTowardsPlayer = false;
+    public EnemyState CurrentState = EnemyState.Patrol;
+    public enum EnemyState
+    {
+        Patrol,
+        Aggro
+    }
     GameObject player;
     public bool ShowHpBar = false;
+    public int AttackRange = 0;
+    public Vector2 GridLocation => new(transform.position.x, transform.position.z);
+
+    public List<Vector2> GetTilesInScoutRange()
+    {
+        List<Vector2> tiles = new List<Vector2>();
+        Vector2 gridLocation = GridLocation;
+
+        // Add the initial tile (range 0 = current tile)
+        tiles.Add(gridLocation);
+
+        for (int x = -AttackRange; x <= AttackRange; x++)
+        {
+            for (int y = -AttackRange; y <= AttackRange; y++)
+            {
+                if (x == 0 && y == 0) continue;
+
+                tiles.Add(new Vector2(gridLocation.x + x, gridLocation.y + y));
+            }
+        }
+
+        return tiles;
+    }
+
+    public void OnTick()
+    {
+        print("Checking attack range");
+        // TODO - Inefficient - use physics or precise colliders(?)
+
+        bool found = false;
+        foreach (Vector2 tile in GetTilesInScoutRange())
+        {
+            if (player.GetComponent<PlayerController>().GridLocation == tile)
+            {
+                found = true;
+            }
+        }
+
+        if (found)
+        {
+            print("Player is in range. now im angy");
+            SetAggressive();
+        }
+        else
+        {
+            print("Player is gone. not angy anymore");
+            SetPatrol();
+        }
+    }
 
     public void Start()
     {
         //TODO
         player = GameObject.FindGameObjectWithTag("Player");
+
+        TickCounter.Instance.OnTick += OnTick;
     }
     public override void Interact()
     {
@@ -35,6 +95,26 @@ public class Enemy : Interactable
         }
     }
 
+    private void SetPatrol()
+    {
+        ShowHpBar = false;
+        AggroTowardsPlayer = true;
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material.color = Color.white;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        foreach (var tile in GetTilesInScoutRange())
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawCube(new(tile.x,0,tile.y), Vector3.one*0.7f);
+        }
+    }
+
     void OnGUI()
     {
         if (ShowHpBar)
@@ -45,6 +125,7 @@ public class Enemy : Interactable
             float hpBarHeight = 5;
             float currentHp = 100;
             float maxHp = 100;
+            
 
             // Draw background (depleted health)
             GUI.color = Color.red;
--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
